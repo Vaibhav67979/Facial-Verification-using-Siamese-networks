babdb1afd3a58e4f0af8882bc22dc6e8c19c6964
Vaibhav P <vaibhav67979@gmail.com>
2023-06-08 22:12:52 +0530

    built basic kivy app

---
+++ app/faceid.py
@@ -0,0 +1,71 @@
+# Import kivy dependencies first
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+# Import kivy UX components
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+
+# Import other kivy stuff
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivy.logger import Logger
+
+# Import other dependencies
+import cv2
+import tensorflow as tf
+from layers import L1Dist
+import os
+import numpy as np
+
+
+# Build app and layout
+class CamApp(App):
+
+    def build(self):
+        # Main layout components
+        self.web_cam = Image(size_hint=(1,.8))
+        self.button = Button(text="Verify", size_hint=(1, .1))
+        self.verification = Label(text="Verification Uninitiated", size_hint=(1, .1))
+
+        # Add items to layout
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(self.web_cam)
+        layout.add_widget(self.button)
+        layout.add_widget(self.verification)
+
+        # Setup video capture device
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.update, 1.0/33.0)
+
+        return layout
+
+    def update(self, *args):
+
+        # Read frame from opencv
+        ret, frame = self.capture.read()
+        frame = frame[120:120+250, 200:200+250, :]
+
+        # Flip horizontal and convert image to texture
+        buf = cv2.flip(frame, 0).tostring()
+        img_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+        img_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        self.web_cam.texture = img_texture
+
+    def preprocess(self, file_path):
+        # Read in image from file path
+        byte_img = tf.io.read_file(file_path)
+        # Load in the image
+        img = tf.io.decode_jpeg(byte_img)
+
+        # Preprocessing steps - resizing the image to be 100x100x3
+        img = tf.image.resize(img, (100, 100))
+        # Scale image to be between 0 and 1
+        img = img / 255.0
+
+        # Return image
+        return img
+
+if __name__ == '__main__':
+    CamApp().run()
